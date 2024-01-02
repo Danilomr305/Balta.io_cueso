ff1bd1cc998a1436d511c4d6ce6b8b52677fd5dd
Danilomr305 <danilomr305@gmail.com>
2024-01-02 13:13:14 -0300

    k

--- todo/lib/pages/home_page.dart
+++ todo/lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 
-// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, prefer_const_constructors, avoid_unnecessary_containers, unused_import, unused_local_variable
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, prefer_const_constructors, avoid_unnecessary_containers, unused_import, unused_local_variable, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:todo/models/item.dart';
@@ -22,13 +22,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+    setState(() {
+      widget.items.add(Item(
+        done: false,
+        title: newTaskCtrl.text,
+      ));
+      newTaskCtrl.clear();
+    });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Todo List"
-        ),
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            )
+          ),
+        )
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
@@ -36,19 +66,34 @@ class _HomePageState extends State<HomePage> {
           final  item = widget.items[index];
 
           return Center(
-            child: CheckboxListTile(
-              title: Text(item.title),
-              key: Key(item.title),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value!;
-                });
+            child: Dismissible(
+              background: Container(
+                color: Colors.red.withOpacity(0.5),
+              ),
+              onDismissed: (direction) {
+                remove(index);
               },
+              key: Key(item.title),
+              child: CheckboxListTile(
+                title: Text(item.title),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value!;
+                  });
+                },
+              ),
             )
           );
         },
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add, 
+        child: Icon(
+          Icons.add
+        ),
+        backgroundColor: Colors.pink,
+      ),
     );
   }
 }
